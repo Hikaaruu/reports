462dd95d7e201c86a3d5d7ff0d2e5aaa3b44bb3f
Valery Polishchuk <valera.polishyk2@gmail.com>
2024-03-13 13:20:04 +0200

    lab-2-updated

--- js/script.js
+++ js/script.js
@@ -8,7 +8,12 @@ document.querySelector('.sidebar').addEventListener('click', function(event) {
                 document.getElementById('main-content').innerHTML = xhr.responseText;
             }
         };
-        var baseUrl = window.location.pathname.endsWith("index.html") ? 'pages/lab-1/' : 'pages/' + window.location.pathname.split('/').slice(-1)[0].replace('.html', '') + '/';
+        var baseUrl;
+        if (window.location.pathname.endsWith("index.html") || window.location.pathname.endsWith("/")) {
+            baseUrl = 'pages/lab-1/';
+        } else {
+            baseUrl = 'pages/' + window.location.pathname.split('/').slice(-1)[0].replace('.html', '') + '/';
+        }
         xhr.open('GET', baseUrl + buttonId.split('-')[1] + '.html', true);
         xhr.send();
     }
