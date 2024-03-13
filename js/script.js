7c1ffee019858d4699bfa381664dab6f086ee906
Valery Polishchuk <valera.polishyk2@gmail.com>
2024-03-13 11:49:00 +0200

    lab-2-added

--- js/script.js
+++ js/script.js
@@ -1,70 +1,15 @@
-document.getElementById('butt-1').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/1.html', true);
-    xhr.send();
-});
-
-document.getElementById('butt-2').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/2.html', true);
-    xhr.send();
-});
-
-document.getElementById('butt-3').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/3.html', true);
-    xhr.send();
-});
-
-document.getElementById('butt-4').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/4.html', true);
-    xhr.send();
-});
-
-document.getElementById('butt-5').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/5.html', true);
-    xhr.send();
-});
-document.getElementById('butt-6').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'pages/lab-1/6.html', true);
-    xhr.send();
+document.querySelector('.sidebar').addEventListener('click', function(event) {
+    if (event.target && event.target.matches('.button')) {
+        event.preventDefault();
+        var buttonId = event.target.id;
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('main-content').innerHTML = xhr.responseText;
+            }
+        };
+        var baseUrl = window.location.pathname.endsWith("index.html") ? 'pages/lab-1/' : 'pages/' + window.location.pathname.split('/').slice(-1)[0].replace('.html', '') + '/';
+        xhr.open('GET', baseUrl + buttonId.split('-')[1] + '.html', true);
+        xhr.send();
+    }
 });
