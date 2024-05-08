e074ba0b6ff2c3ab15f139ca3f76cc34b80f408b
Valery Polishchuk <valera.polishyk2@gmail.com>
2024-05-08 06:09:35 +0300

    lab 6 added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.querySelector('.sidebar').addEventListener('click', function(event) {
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 document.getElementById('main-content').innerHTML = xhr.responseText;
+                addScriptIfNotExists(buttonId);
             }
         };
         var baseUrl;
@@ -17,8 +18,37 @@ document.querySelector('.sidebar').addEventListener('click', function(event) {
         xhr.open('GET', baseUrl + buttonId.split('-')[1] + '.html', true);
         xhr.send();
 
-        var script = document.createElement('script');
-        script.src = baseUrl + 'js/code.js';
-        document.head.appendChild(script);
+        //var script = document.createElement('script');
+        //script.src = baseUrl + buttonId.split('-')[1] + '/code.js';
+        //document.head.appendChild(script);
     }
 });
+
+function addScriptIfNotExists(buttonId) {
+    var baseUrl;
+    if (window.location.pathname.endsWith("index.html") || window.location.pathname.endsWith("/")) {
+        baseUrl = 'pages/lab-1/';
+    } else {
+        baseUrl = 'pages/' + window.location.pathname.split('/').slice(-1)[0].replace('.html', '') + '/';
+    }
+    var scriptSrc = baseUrl + buttonId.split('-')[1] + '/code.js';
+    
+    // Remove all scripts except 'js/script.js'
+    var scripts = document.querySelectorAll('script:not([src="js/script.js"])');
+    scripts.forEach(function(script) {
+        script.remove();
+    });
+
+    // Add the new script if it doesn't exist
+    if (!document.querySelector('script[src="' + scriptSrc + '"]')) {
+        var script = document.createElement('script');
+        script.src = scriptSrc;
+        script.onload = function() {
+            console.log("Script loaded successfully");
+        };
+        script.onerror = function() {
+            console.error("Failed to load script: " + scriptSrc);
+        };
+        document.head.appendChild(script);
+    }
+}
